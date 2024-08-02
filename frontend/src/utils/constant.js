03cda132cd2e8b82637d2cab9b2f55a1f3f349d4
charan <charan4567890@gmail.com>
2024-08-02 10:30:41 +0530

    update

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+export const USER_API_END_POINT = process.env.REACT_APP_USER_API_END_POINT;
+export const JOB_API_END_POINT = process.env.REACT_APP_JOB_API_END_POINT;
+export const APPLICATION_API_END_POINT = process.env.REACT_APP_APPLICATION_API_END_POINT;
+export const COMPANY_API_END_POINT = process.env.REACT_APP_COMPANY_API_END_POINT;
